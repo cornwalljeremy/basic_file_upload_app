2fb3328d34e40731c6233141d363c9268a89f1e9
cornwalljeremy <cornwall.jeremy@gmail.com>
2026-01-13 12:45:15 -0700

    Removed next from git

--- .next/dev/server/app/api/upload/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/upload/route.js")
-R.c("server/chunks/[root-of-the-server]__339dee80._.js")
-R.c("server/chunks/[root-of-the-server]__cc062ccd._.js")
-R.c("server/chunks/_next-internal_server_app_api_upload_route_actions_f15ed185.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/upload/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/upload/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
